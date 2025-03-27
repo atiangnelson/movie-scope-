8f5b6901af094faae90b5a44183ade87ee0370d1
Nelson <nelson.atiang@student.moringaschool.com>
2025-03-27 17:39:04 +0300

    worked on the movie trailers

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ document.addEventListener("DOMContentLoaded",fetchMovies)
 let url="http://localhost:3000/movies"
 
 function fetchMovies (){
+
+
 fetch(url)
 .then(res=>res.json())
 .then((info)=>{
     displayMovieInfo(info)
-    displayMoviePoster(info)
+    
 })
 .catch(error=>console.log("Error fetching movies:",error))
 
@@ -19,38 +21,17 @@ function displayMovieInfo(movies) {
         const moviecard=document.createElement("div")
         moviecard.classList.add("movie-card")
         moviecard.innerHTML=`
+        <a href="${movie.trailer}"><img src="${movie.image}" alt="" id="movieimg">$</a>
                 <h3>${movie.title}</h3>
                 <p>${movie.synopsis}</p>
                 <p>${movie.keywords}</p>
                 <p>${movie.genres}</p>
                 <p>${movie.streaming}</p>
-                <h4>${movie.year}</h4>`
+                <h4>${movie.year}</h4>
+                
+                `
         infobox.appendChild(moviecard)
-    
-        // const information=document.getElementById("title")
-        // information.textContent=movie.title
-        // const detail=document.getElementById("movieinfo")
-        // detail.textContent=movie.synopsis
-        // const keyword=document.getElementById("keywords")
-        // keyword.textContent=movie.keywords
-        // const type=document.getElementById("genres")
-        // type.textContent=movie.genres
-        // const availability=document.getElementById("stream")
-        // availability.textContent=movie.streaming
-        // const year=document.getElementById("year")
-        // year.textContent=movie.year
         
     }  
     )
     };
-
- function displayMoviePoster(posters){
-    const imageinfo=document.getElementById("imginfo")
-    imageinfo.innerHTML=""
- posters.forEach((movie)=>{
-     const image=document.createElement("img")
-     image.src=movie.image
-     image.alt=movie.title
-     imageinfo.appendChild(image)
- })
- }
