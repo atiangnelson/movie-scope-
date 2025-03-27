13732a2ada3833006eedec770aebd213c2ad72c0
Nelson <nelson.atiang@student.moringaschool.com>
2025-03-27 15:42:40 +0300

    changed my html structure to prevent overwriting

--- js/index.js
+++ js/index.js
@@ -13,38 +13,44 @@ fetch(url)
 
 }
 function displayMovieInfo(movies) {
-    
-    
+     const infobox=document.getElementById("infobar")
+     infobox.innerHTML=""
     movies.forEach((movie) => {
+        const moviecard=document.createElement("div")
+        moviecard.classList.add("movie-card")
+        moviecard.innerHTML=`
+                <h3>${movie.title}</h3>
+                <p>${movie.synopsis}</p>
+                <p>${movie.keywords}</p>
+                <p>${movie.genres}</p>
+                <p>${movie.streaming}</p>
+                <h4>${movie.year}</h4>`
+        infobox.appendChild(moviecard)
+    
+        // const information=document.getElementById("title")
+        // information.textContent=movie.title
+        // const detail=document.getElementById("movieinfo")
+        // detail.textContent=movie.synopsis
+        // const keyword=document.getElementById("keywords")
+        // keyword.textContent=movie.keywords
+        // const type=document.getElementById("genres")
+        // type.textContent=movie.genres
+        // const availability=document.getElementById("stream")
+        // availability.textContent=movie.streaming
+        // const year=document.getElementById("year")
+        // year.textContent=movie.year
         
-        const infobox=document.getElementById("infobar")
-        const information=document.getElementById("title")
-        information.textContent=movie.title
-        const detail=document.getElementById("movieinfo")
-        detail.textContent=movie.synopsis
-        const keyword=document.getElementById("keywords")
-        keyword.textContent=movie.keywords
-        const type=document.getElementById("genres")
-        type.textContent=movie.genres
-        const availability=document.getElementById("stream")
-        availability.textContent=movie.streaming
-        const year=document.getElementById("year")
-        year.textContent=movie.year
-        infobox.appendChild(information)
-        infobox.appendChild(detail)
-        infobox.appendChild(keyword)
-        infobox.appendChild(type)
-        infobox.appendChild(availability)
-        infobox.appendChild(year)
-
     }  
     )
     };
 
  function displayMoviePoster(posters){
+    const imageinfo=document.getElementById("imginfo")
+    imageinfo.innerHTML=""
  posters.forEach((movie)=>{
-     const image=document.getElementById("movieimg")
+     const image=document.createElement("img")
      image.src=movie.image
      image.alt=movie.title
+     imageinfo.appendChild(image)
  })
  }
